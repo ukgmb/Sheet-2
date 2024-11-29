64a4da52dca093ff20f7f8bc372e60a7e9db69af
ukgmb <ukgmb@student.kit.edu>
2024-11-30 00:03:17 +0100

    lol

--- Sheet/src/sheet_three/Game.java
+++ Sheet/src/sheet_three/Game.java
@@ -2,7 +2,11 @@ package sheet_three;
 
 import java.util.Scanner;
 
-public class Game {
+public final class Game {
+
+    private Game() {
+
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
